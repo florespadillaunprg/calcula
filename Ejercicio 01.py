22f1bde4f9630bbc020544df6b286f4cea9f196f
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-11-06 20:02:08 -0500

    Alex

---
+++ Ejercicio 01.py
@@ -0,0 +1,16 @@
+# encotrar el divedendo a partir del algoritmo de la divison
+dividendo,divisor,cociente,residuo=0,0,0,0
+
+# asignacion de valores
+divisor=10
+cociente=5
+residuo=0
+
+#calculo
+dividendo=(divisor*cociente)+residuo
+
+# mostrar valores
+print("divisor =", divisor)
+print("cociente =", cociente)
+print("residuo =", residuo)
+print("dividendo =", dividendo)
